2601d3fc1b4b2e617b99b5c5a254be2d821c908e
Norbyte <infernorb@gmail.com>
2019-09-03 18:19:11 +0200

    Implement FixedStringMapBase hashmap lookup

---
+++ OsiInterface/DivBaseTypes.h
@@ -0,0 +1,246 @@
+#pragma once
+
+#include <cstdint>
+#include <array>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+#include <cassert>
+#include <optional>
+
+namespace osidbg
+{
+#pragma pack(push, 1)
+	template <class T>
+	struct PrimitiveSet
+	{
+		T * Buf;
+		uint32_t AllocationSize;
+		uint32_t ItemCount;
+	};
+
+	struct FixedString
+	{
+		inline FixedString()
+			: Str(nullptr)
+		{}
+
+		inline FixedString(char const * s)
+			: Str(s)
+		{}
+
+		inline FixedString(FixedString const & fs)
+			: Str(fs.Str)
+		{}
+
+		inline operator bool() const
+		{
+			return Str != nullptr;
+		}
+
+		char const * Str;
+	};
+
+	FixedString ToFixedString(const char * s);
+
+	template <class ValueType>
+	struct FixedStringMapBase
+	{
+		struct Node
+		{
+			Node * Next;
+			FixedString Key;
+			ValueType Value;
+		};
+
+		uint32_t HashSize;
+		uint32_t _Unused;
+		Node ** HashTable;
+		uint32_t ItemCount;
+
+		ValueType * Find(char const * str) const
+		{
+			auto fs = ToFixedString(str);
+			if (fs) {
+				auto item = HashTable[(uint64_t)fs.Str % HashSize];
+				while (item != nullptr) {
+					if (strcmp(str, item->Key.Str) == 0) {
+						return &item->Value;
+					}
+
+					item = item->Next;
+				}
+			}
+
+			return nullptr;
+		}
+
+		FixedString * FindByValue(ValueType const & value) const
+		{
+			for (uint32_t bucket = 0; bucket < HashSize; bucket++) {
+				Node * item = HashTable[bucket];
+				while (item != nullptr) {
+					if (value == item->Value) {
+						return &item->Key;
+					}
+
+					item = item->Next;
+				}
+			}
+
+			return nullptr;
+		}
+	};
+
+	struct ObjectSet
+	{
+		void * Unknown;
+		void * Buf;
+		uint32_t Capacity;
+		uint32_t Size;
+		void * Unknown2;
+	};
+
+	struct TranslatedString
+	{
+		void * Unknown;
+		char const * String;
+		char const * TranslatedStringKey;
+	};
+
+	struct STDWString
+	{
+		uint64_t Unkn[4];
+	};
+
+	template <class T>
+	struct Array
+	{
+		void * VMT;
+		T * Buf;
+		uint32_t Capacity;
+		uint32_t Size;
+		uint32_t Unkn[2];
+	};
+
+	struct ObjectHandle
+	{
+		uint64_t Handle;
+
+		inline ObjectHandle()
+			: Handle(0)
+		{}
+
+		inline ObjectHandle(uint64_t handle)
+			: Handle(handle)
+		{}
+
+		inline ObjectHandle(uint64_t type, uint64_t index, uint64_t salt)
+		{
+			if (type >= 0x400 || salt >= 0x400000) {
+				Fail("ObjectHandle type/salt overflow.");
+			}
+
+			Handle = index | (salt << 32) | (type << 54);
+		}
+
+		inline ObjectHandle(ObjectHandle const & oh)
+			: Handle(oh.Handle)
+		{}
+
+		inline ObjectHandle & operator = (ObjectHandle const & oh)
+		{
+			Handle = oh.Handle;
+			return *this;
+		}
+
+		inline bool operator == (ObjectHandle const & oh) const
+		{
+			return Handle == oh.Handle;
+		}
+
+		inline uint32_t GetType() const
+		{
+			return Handle >> 54;
+		}
+
+		inline uint32_t GetSalt() const
+		{
+			return (Handle >> 32) & 0x3fffff;
+		}
+
+		inline uint32_t GetIndex() const
+		{
+			return (uint32_t)(Handle & 0xffffffff);
+		}
+
+		inline operator bool() const
+		{
+			return Handle != 0;
+		}
+
+		explicit inline operator int64_t() const
+		{
+			return (int64_t)Handle;
+		}
+	};
+
+	struct ObjectFactory
+	{
+		void * VMT;
+		Array<void *> CharPtrArray;
+		Array<unsigned int> IntArray;
+		void * unkn;
+		uint32_t unkn2[4];
+		PrimitiveSet<void *> CharPrimitives;
+		uint64_t unkn3[3];
+	};
+
+	struct Component
+	{
+		virtual void VMT00() = 0;
+		virtual void VMT08() = 0;
+		virtual void VMT10() = 0;
+		virtual void VMT18() = 0;
+		virtual void * FindComponentByHandle(ObjectHandle const * oh) = 0;
+		virtual void VMT28() = 0;
+		virtual void * FindComponentByGuid(FixedString const * fs) = 0;
+	};
+
+	struct ComponentHandle
+	{
+		uint64_t TypeId;
+		ObjectHandle Handle;
+	};
+
+	struct BaseComponent
+	{
+		void * VMT;
+		ObjectHandle Handle;
+		ComponentHandle Component;
+	};
+
+	struct GlobalStringTable
+	{
+		static bool UseMurmur;
+
+		struct Entry
+		{
+			const char * StringPtrs[10];
+			Entry * Next;
+			uint32_t StringPtrItems;
+			uint32_t Unused;
+
+			uint32_t Count() const;
+			char const * Get(uint32_t i) const;
+		};
+
+		Entry HashTable[65521];
+
+		const char * Find(char const * s, uint64_t length) const;
+		static uint32_t Hash(char const * s, uint64_t length);
+	};
+#pragma pack(pop)
+
+}
--- OsiInterface/DivInterface.h
+++ OsiInterface/DivInterface.h
@@ -1,117 +1,11 @@
 #pragma once
 
-#include <cstdint>
-#include <array>
-#include <vector>
-#include <set>
-#include <map>
-#include <string>
-#include <cassert>
-#include <optional>
+#include "DivBaseTypes.h"
 
 namespace osidbg
 {
 
 #pragma pack(push, 1)
-	template <class T>
-	struct PrimitiveSet
-	{
-		T * Buf;
-		uint32_t AllocationSize;
-		uint32_t ItemCount;
-	};
-
-	struct FixedString
-	{
-		inline FixedString()
-			: Str(nullptr)
-		{}
-
-		inline FixedString(char const * s)
-			: Str(s)
-		{}
-
-		inline FixedString(FixedString const & fs)
-			: Str(fs.Str)
-		{}
-
-		inline operator bool() const
-		{
-			return Str != nullptr;
-		}
-
-		char const * Str;
-	};
-
-	template <class ValueType>
-	struct FixedStringMapBase
-	{
-		struct Node
-		{
-			Node * Next;
-			FixedString Key;
-			ValueType Value;
-		};
-
-		uint32_t HashSize;
-		uint32_t _Unused;
-		Node ** HashTable;
-		uint32_t ItemCount;
-
-		ValueType * Find(char const * str) const
-		{
-			for (uint32_t bucket = 0; bucket < HashSize; bucket++) {
-				Node * item = HashTable[bucket];
-				while (item != nullptr) {
-					if (strcmp(str, item->Key.Str) == 0) {
-						return &item->Value;
-					}
-
-					item = item->Next;
-				}
-			}
-
-			return nullptr;
-		}
-
-		FixedString * FindByValue(ValueType const & value) const
-		{
-			for (uint32_t bucket = 0; bucket < HashSize; bucket++) {
-				Node * item = HashTable[bucket];
-				while (item != nullptr) {
-					if (value == item->Value) {
-						return &item->Key;
-					}
-
-					item = item->Next;
-				}
-			}
-
-			return nullptr;
-		}
-	};
-
-	struct ObjectSet
-	{
-		void * Unknown;
-		void * Buf;
-		uint32_t Capacity;
-		uint32_t Size;
-		void * Unknown2;
-	};
-
-	struct TranslatedString
-	{
-		void * Unknown;
-		char const * String;
-		char const * TranslatedStringKey;
-	};
-
-	struct STDWString
-	{
-		uint64_t Unkn[4];
-	};
-
 	struct CRPGStats_Object
 	{
 		void * VMT;
@@ -234,89 +128,6 @@ namespace osidbg
 		bool SetAttributeInt(CRPGStats_Object * object, const char * attributeName, int32_t value);
 	};
 
-	template <class T>
-	struct Array
-	{
-		void * VMT;
-		T * Buf;
-		uint32_t Capacity;
-		uint32_t Size;
-		uint32_t Unkn[2];
-	};
-
-	struct ObjectHandle
-	{
-		uint64_t Handle;
-
-		inline ObjectHandle()
-			: Handle(0)
-		{}
-
-		inline ObjectHandle(uint64_t handle)
-			: Handle(handle)
-		{}
-
-		inline ObjectHandle(uint64_t type, uint64_t index, uint64_t salt)
-		{
-			if (type >= 0x400 || salt >= 0x400000) {
-				Fail("ObjectHandle type/salt overflow.");
-			}
-
-			Handle = index | (salt << 32) | (type << 54);
-		}
-
-		inline ObjectHandle(ObjectHandle const & oh)
-			: Handle(oh.Handle)
-		{}
-
-		inline ObjectHandle & operator = (ObjectHandle const & oh)
-		{
-			Handle = oh.Handle;
-			return *this;
-		}
-
-		inline bool operator == (ObjectHandle const & oh) const
-		{
-			return Handle == oh.Handle;
-		}
-
-		inline uint32_t GetType() const
-		{
-			return Handle >> 54;
-		}
-
-		inline uint32_t GetSalt() const
-		{
-			return (Handle >> 32) & 0x3fffff;
-		}
-
-		inline uint32_t GetIndex() const
-		{
-			return (uint32_t)(Handle & 0xffffffff);
-		}
-
-		inline operator bool() const
-		{
-			return Handle != 0;
-		}
-
-		explicit inline operator int64_t() const
-		{
-			return (int64_t)Handle;
-		}
-	};
-
-	struct ObjectFactory
-	{
-		void * VMT;
-		Array<void *> CharPtrArray;
-		Array<unsigned int> IntArray;
-		void * unkn;
-		uint32_t unkn2[4];
-		PrimitiveSet<void *> CharPrimitives;
-		uint64_t unkn3[3];
-	};
-
 	struct NetworkObjectFactory : public ObjectFactory
 	{
 		FixedStringMapBase<void *> CharacterMap;
@@ -331,17 +142,6 @@ namespace osidbg
 		uint64_t Unkn8;
 	};
 
-	struct Component
-	{
-		virtual void VMT00() = 0;
-		virtual void VMT08() = 0;
-		virtual void VMT10() = 0;
-		virtual void VMT18() = 0;
-		virtual void * FindComponentByHandle(ObjectHandle const * oh) = 0;
-		virtual void VMT28() = 0;
-		virtual void * FindComponentByGuid(FixedString const * fs) = 0;
-	};
-
 	struct ComponentEntry
 	{
 		// todo
@@ -353,19 +153,6 @@ namespace osidbg
 	{
 		uint64_t Unkn[17];
 		Array<ComponentEntry> Components;
-	};
-
-	struct ComponentHandle
-	{
-		uint64_t TypeId;
-		ObjectHandle Handle;
-	};
-
-	struct BaseComponent
-	{
-		void * VMT;
-		ObjectHandle Handle;
-		ComponentHandle Component;
 	};
 
 	struct CharacterFactory : public NetworkObjectFactory
@@ -905,27 +692,6 @@ namespace osidbg
 		float Lifetime;
 		bool IsTotem;
 		bool MapToAiGrid;
-	};
-
-	struct GlobalStringTable
-	{
-		static bool UseMurmur;
-
-		struct Entry
-		{
-			const char * StringPtrs[10];
-			Entry * Next;
-			uint32_t StringPtrItems;
-			uint32_t Unused;
-
-			uint32_t Count() const;
-			char const * Get(uint32_t i) const;
-		};
-
-		Entry HashTable[65521];
-
-		const char * Find(char const * s, uint64_t length) const;
-		static uint32_t Hash(char const * s, uint64_t length);
 	};
 #pragma pack(pop)
 
