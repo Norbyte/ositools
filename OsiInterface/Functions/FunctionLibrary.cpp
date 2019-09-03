560e6c3851564bc181aff691d1a310d50a65e337
Norbyte <infernorb@gmail.com>
2019-09-03 20:22:02 +0200

    Add SkillSetCooldown Osiris call

--- OsiInterface/Functions/FunctionLibrary.cpp
+++ OsiInterface/Functions/FunctionLibrary.cpp
@@ -20,7 +20,53 @@ namespace osidbg
 				args.Get(1).String = const_cast<char *>(item->StatsId.Str);
 				return true;
 			}
-			
+		}
+
+		void SkillSetCooldown(OsiArgumentDesc const & args)
+		{
+			auto characterGuid = args.Get(0).String;
+			auto character = FindCharacterByNameGuid(characterGuid);
+			if (character == nullptr) {
+				OsiError("SkillSetCooldown(): Character '" << characterGuid << "' does not exist!");
+				return;
+			}
+
+			if (character->SkillManager == nullptr) {
+				OsiError("SkillSetCooldown(): Character '" << characterGuid << "' has no SkillManager!");
+				return;
+			}
+
+			auto skillId = args.Get(1).String;
+			auto skill = character->SkillManager->Skills.Find(skillId);
+			if (skill == nullptr) {
+				OsiError("SkillSetCooldown(): Character '" << characterGuid << "' doesn't have skill '" << skillId << "'!");
+				return;
+			}
+
+			if ((*skill)->OncePerCombat) {
+				OsiError("SkillSetCooldown(): Skill '" << skillId << " doesn't support cooldown!");
+				return;
+			}
+
+			auto cooldown = args.Get(2).Float;
+			if (cooldown < 0.0f) {
+				OsiError("SkillSetCooldown(): Cooldown cannot be negative!");
+				return;
+			}
+
+			(*skill)->ActiveCooldown = cooldown;
+		}
+
+		void BreakOnCharacter(OsiArgumentDesc const & args)
+		{
+			auto character = FindCharacterByNameGuid(args.Get(0).String);
+			OsiWarn("GotIt");
+		}
+
+		void BreakOnItem(OsiArgumentDesc const & args)
+		{
+			auto item = FindItemByNameGuid(args.Get(0).String);
+			OsiWarn("GotIt");
 		}
 
 		void DoExperiment(OsiArgumentDesc const & args)
@@ -52,6 +98,38 @@ namespace osidbg
 			&func::ItemGetStatsId
 		);
 		functionMgr.Register(std::move(itemGetStatsId));
+
+
+		auto skillSetCooldown = std::make_unique<CustomCall>(
+			"NRD_SkillSetCooldown",
+			std::vector<CustomFunctionParam>{
+				{ "Character", ValueType::GuidString, FunctionArgumentDirection::In },
+				{ "SkillId", ValueType::String, FunctionArgumentDirection::In },
+				{ "Cooldown", ValueType::Real, FunctionArgumentDirection::In }
+			},
+			&func::SkillSetCooldown
+		);
+		functionMgr.Register(std::move(skillSetCooldown));
+
+
+		auto breakOnCharacter = std::make_unique<CustomCall>(
+			"NRD_BreakOnCharacter",
+			std::vector<CustomFunctionParam>{
+				{ "Character", ValueType::GuidString, FunctionArgumentDirection::In }
+			},
+			&func::BreakOnCharacter
+		);
+		functionMgr.Register(std::move(breakOnCharacter));
+
+
+		auto breakOnItem = std::make_unique<CustomCall>(
+			"NRD_BreakOnItem",
+			std::vector<CustomFunctionParam>{
+				{ "Item", ValueType::GuidString, FunctionArgumentDirection::In }
+			},
+			&func::BreakOnItem
+		);
+		functionMgr.Register(std::move(breakOnItem));
 
 
 		auto experiment = std::make_unique<CustomCall>(
