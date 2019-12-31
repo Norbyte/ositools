62c0d13c6912ab39ffd86f86c69c0284dcc846da
Norbyte <infernorb@gmail.com>
2019-12-31 10:51:29 +0100

    Display WinInet error messages properly

--- OsiLoader/Utils.cpp
+++ OsiLoader/Utils.cpp
@@ -164,8 +164,14 @@ std::string GetLastErrorString(DWORD lastError)
 	if (lastError == 0) return "";
 
 	LPSTR messageBuffer = nullptr;
-	size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-		NULL, lastError, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
+	size_t size = FormatMessageA(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM 
+		| FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_FROM_HMODULE,
+		LoadLibrary(_T("wininet.dll")), 
+		lastError, 
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), 
+		(LPSTR)&messageBuffer, 
+		0, NULL);
 
 	std::string message(messageBuffer, size);
 	LocalFree(messageBuffer);
