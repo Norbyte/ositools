896a209fcd3f0044b296f51ac0b14854b8d1c1ee
Norbyte <infernorb@gmail.com>
2020-01-05 19:50:30 +0100

    Release v38

--- OsiInterface/Version.h
+++ OsiInterface/Version.h
@@ -3,8 +3,8 @@
 #include <cstdint>
 
 namespace osidbg {
-	#define RES_DLL_VERSION "37.0.0.0"
-	#define RES_DLL_MAJOR_VERSION 37
+	#define RES_DLL_VERSION "38.0.0.0"
+	#define RES_DLL_MAJOR_VERSION 38
 
 	static constexpr uint32_t CurrentVersion = RES_DLL_MAJOR_VERSION;
 }
