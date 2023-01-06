54b34485545c25369f8763c1c35f482f9a80135b
Norbyte <infernorb@gmail.com>
2023-01-06 20:51:32 +0100

    Fix proto build script

--- generate-proto.bat
+++ generate-proto.bat
@@ -1,5 +1,10 @@
-cd OsiInterface
-..\External\x64-windows\tools\protobuf\protoc --cpp_out=. osidebug.proto
-..\External\x64-windows\tools\protobuf\protoc --cpp_out=. LuaDebug.proto
-..\External\x64-windows\tools\protobuf\protoc --cpp_out=. ScriptExtensions.proto
+cd ScriptExtender\Extender\Shared
+..\..\..\External\x64-windows\tools\protobuf\protoc --cpp_out=. ScriptExtensions.proto
+
+cd ..\..\Lua\Debugger
+..\..\..\External\x64-windows\tools\protobuf\protoc --cpp_out=. LuaDebug.proto
+
+cd ..\..\Osiris\Debugger
+..\..\..\External\x64-windows\tools\protobuf\protoc --cpp_out=. osidebug.proto
+
 pause
